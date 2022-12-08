49feb61aa989d2430506554cc19dae33788ca919
Sanoj Oad <41338080+sanojkumaroad@users.noreply.github.com>
2022-12-08 15:16:12 -0600

    Added assignment files

---
+++ 01-introgitgithub-sanojkumaroad/introduction.cc
@@ -0,0 +1,23 @@
+/*      Sanoj Oad
+	z1980626
+	CSCI 340 3 
+	
+	I certify that this is my own work and where appropriate an extension 
+	of the starter code provided for the assignment.
+*/
+#include <iostream>
+
+using namespace std;
+
+int
+main(int argc, char *argv[])
+{
+
+	// Insert new code line replacing this comment.
+	
+	cout << "I'm very excited to be taking Data Structures" << endl;
+        cout << "Data Structures are Important!!" << endl;   // Yes, I know there is a typo!!!
+        
+	return 0;
+
+}// End of main() routine.
